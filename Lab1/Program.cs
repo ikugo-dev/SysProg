430251a89a59d24a764102bbd9e25e3b40f4b70a
Aleksandar Ristic <aleksandarristic503@gmail.com>
2025-07-21 22:42:59 +0200

    dodao logger, ne samo response

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -5,29 +5,33 @@ var app = builder.Build();
 
 app.MapGet("/", () => Results.NotFound("Please provide an input file"));
 
+IResult ReturnResponse(string message, string returnType)
+{
+    Console.WriteLine(message);
+    if (returnType == "PROBLEM") return Results.Problem(message);
+    if (returnType == "NOT_FOUND") return Results.NotFound(message);
+    if (returnType == "OK") return Results.Ok(message);
+    return Results.BadRequest("Unknown return type");
+}
+
 app.MapGet("/{inputFileName}", (string inputFileName) =>
 {
     if (inputFileName.Trim() == "")
-    {
-        return Results.Problem("File name can't be empty");
-    }
+        return ReturnResponse("File name can't be empty", "PROBLEM");
     if (!File.Exists(inputFileName))
-    {
-        return Results.NotFound($"File {inputFileName} doesn't exist");
-    }
+        return ReturnResponse($"File {inputFileName} doesn't exist", "NOT_FOUND");
 
     byte[] inputBytes = File.ReadAllBytes(inputFileName);
     if (inputBytes.Length == 0)
-    {
-        return Results.Problem("File can't be empty");
-    }
+        return ReturnResponse("File name can't be empty", "PROBLEM");
+
     byte[] inputBytesHashed = SHA256.HashData(inputBytes);
     string hashedFileText = Convert.ToHexString(inputBytesHashed);
 
     string outputFileName = inputFileName + "-hashed";
     File.WriteAllText(outputFileName, hashedFileText);
 
-    return Results.Ok($"Hashed file: {inputFileName} => {outputFileName}");
+    return ReturnResponse($"Hashed file: {inputFileName} => {outputFileName}", "OK");
 });
 
 app.Run();
