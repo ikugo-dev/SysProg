b8ebb5bb92596d7afdffa1585fe012054551667a
Aleksandar Ristic <aleksandarristic503@gmail.com>
2025-10-23 15:32:06 +0200

    changed to HttpListener model

---
+++ SysProj1/Handler.cs
@@ -0,0 +1,69 @@
+using System.Net;
+using System.Security.Cryptography;
+using System.Collections.Concurrent;
+
+public class Handler
+{
+    private readonly ConcurrentDictionary<string, string> _cache = new();
+    private readonly object _lockObj = new();
+
+    public void Handle(HttpListenerContext context)
+    {
+        if (context.Request.HttpMethod != "GET")
+        {
+            WriteResponse(context.Response, 400, "Only GET requests are supported.");
+            return;
+        }
+
+        string path = context.Request.Url!.AbsolutePath;
+        if (!path.StartsWith("/api/"))
+        {
+            WriteResponse(context.Response, 400, "Api calls start with /api/{inputFileName}");
+            return;
+        }
+
+        string inputFileName = path.Remove(0, 5);
+        if (string.IsNullOrWhiteSpace(inputFileName))
+        {
+            WriteResponse(context.Response, 400, "File name can't be empty.");
+            return;
+        }
+
+        if (!File.Exists(inputFileName)) // pretvori u rekurzivnu proveru ?
+        {
+            WriteResponse(context.Response, 404, $"File {inputFileName} doesn't exist.");
+            return;
+        }
+
+        if (_cache.TryGetValue(inputFileName, out string? cachedHash))
+        {
+            WriteResponse(context.Response, 200, $"Cached hash: {inputFileName} => {cachedHash}");
+            return;
+        }
+
+        string hashedFileText;
+        lock (_lockObj)
+        {
+            byte[] inputBytes = File.ReadAllBytes(inputFileName);
+            if (inputBytes.Length == 0)
+            {
+                WriteResponse(context.Response, 404, "File can't be empty");
+                return;
+            }
+            byte[] inputBytesHashed = SHA256.HashData(inputBytes);
+            hashedFileText = Convert.ToHexString(inputBytesHashed);
+            _cache[inputFileName] = hashedFileText;
+        }
+
+        WriteResponse(context.Response, 200, $"Hashed file: {inputFileName} => {hashedFileText}");
+    }
+
+    private static void WriteResponse(HttpListenerResponse response, int statusCode, string message)
+    {
+        response.StatusCode = statusCode;
+        using var writer = new StreamWriter(response.OutputStream);
+        writer.Write(message);
+        writer.Flush();
+        response.Close();
+    }
+}
---
+++ SysProj1/Server.cs
@@ -0,0 +1,49 @@
+using System.Net;
+
+public class Server
+{
+    private bool _isRunning;
+    private readonly HttpListener _listener = new();
+    private Thread? _listenerThread;
+    private readonly Action<HttpListenerContext> _processRequest;
+
+    public Server(Action<HttpListenerContext> processRequest) => _processRequest = processRequest;
+
+    public void Start(string port = "5050")
+    {
+        if (_isRunning) return;
+
+        _listener.Prefixes.Add($"http://localhost:{port}/");
+        _listener.Start();
+        _isRunning = true;
+        _listenerThread = new Thread(ListenLoop);
+        _listenerThread.Start();
+        Console.WriteLine($"Server started on port {port}");
+    }
+
+    public void Stop()
+    {
+        if (!_isRunning) return;
+        _isRunning = false;
+        _listener.Stop();
+        _listenerThread?.Join();
+        Console.WriteLine("Server stopped");
+    }
+
+    private void ListenLoop()
+    {
+        try
+        {
+            while (_isRunning)
+            {
+                var context = _listener.GetContext();
+                ThreadPool.QueueUserWorkItem(_ => _processRequest(context));
+            }
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Listen loop has stopped");
+        }
+    }
+}
+
